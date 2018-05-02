f7b31e19b5cf20a14604c35f1dd172accce7d9b3
yhylord <yhylord@gmail.com>
2018-05-02 23:03:01 +0800

    Add basic operations for file

--- src/app/durandal.cpp
+++ src/app/durandal.cpp
@@ -1,6 +1,8 @@
 #include "durandal.h"
 #include "ui_durandal.h"
 
+#include <QtWidgets>
+
 Durandal::Durandal(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Durandal)
@@ -11,4 +13,82 @@ Durandal::Durandal(QWidget *parent) :
 Durandal::~Durandal()
 {
     delete ui;
+}
+
+void Durandal::on_actionNew_triggered()
+{
+    currentFilename.clear();
+    ui->plainTextEdit->setPlainText(QString());
+}
+
+void Durandal::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, tr("Open File"));
+    QFile file(filename);
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Warning"), tr("Cannot open file: ") + file.errorString());
+        return;
+    }
+    currentFilename = filename;
+    setWindowTitle(filename);
+    // TODO: Read file in real-time
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->plainTextEdit->setPlainText(text);
+    file.close();
+}
+
+bool Durandal::on_actionSave_triggered()
+{
+    if (currentFilename.isEmpty()) {
+        return on_actionSaveAs_triggered();
+    }
+    return saveFile(currentFilename);
+}
+
+bool Durandal::on_actionSaveAs_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, tr("Save File"));
+    currentFilename = filename;
+    return saveFile(filename);
+}
+
+void Durandal::on_actionQuit_triggered()
+{
+    if (maybeSave()) {
+        QApplication::quit();
+    }
+}
+
+bool Durandal::saveFile(const QString &filename) {
+    QFile file(filename);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, tr("Warning"), tr("Cannot save file: ") + file.errorString());
+        return false;
+    }
+    QTextStream out(&file);
+    QString text = ui->plainTextEdit->toPlainText();
+    out << text;
+    file.close();
+    return true;
+}
+
+bool Durandal::maybeSave() {
+    if (!ui->plainTextEdit->document()->isModified()) {
+        return true;
+    }
+    const QMessageBox::StandardButton ret =
+            QMessageBox::warning(this, tr("Warning"),
+                                 tr("This document has been modified\n"
+                                    "Do you Want to save your changes?"),
+                                 QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+    switch (ret) {
+    case QMessageBox::Save:
+        return on_actionSave_triggered();
+    case QMessageBox::Discard:
+        return true;
+    default:
+        break;
+    }
+    return false;
 }
--- src/app/durandal.h
+++ src/app/durandal.h
@@ -15,8 +15,23 @@ public:
     explicit Durandal(QWidget *parent = 0);
     ~Durandal();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    bool on_actionSave_triggered();
+
+    bool on_actionSaveAs_triggered();
+
+    void on_actionQuit_triggered();
+
 private:
     Ui::Durandal *ui;
+    QString currentFilename;
+
+    bool saveFile(const QString &filename);
+    bool maybeSave();
 };
 
 #endif // DURANDAL_H
